11f3fcd70b9e3c2e70a808546175e5328d12ee14
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-20 15:06:00 +0300

    add layout for monile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,17 @@ import mobileBg from "../../public/images/bg-sidebar-mobile.svg"
 import Image from "next/image";
 
 const Header = () => {
+    
+
     return ( 
         <div className="flex justify-center items-center ">
             <Image src={mobileBg} alt="back ground for sideBar" className="w-full x z-0 absolute"/>
-            <p className="z-10 text-lg mt-5">test Test test</p>
+            <div className="z-10 flex text-lg mt-7">
+                <div className="text-White font-Regular text-sm border-White border mx-2 py-1 px-2.5 rounded-full">1</div>
+                <div className="text-White font-Regular text-sm border-White border mx-2 py-1 px-2.5 rounded-full">2</div>
+                <div className="text-White font-Regular text-sm border-White border mx-2 py-1 px-2.5 rounded-full">3</div>
+                <div className="text-White font-Regular text-sm border-White border mx-2 py-1 px-2.5 rounded-full">4</div>
+            </div>
         </div>
         
 
