0b80cf007142eefb60fc96814549252ef28e5e11
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-23 01:39:04 +0300

    refactoring for getTotal funtion TODO make a map from basePriceMap

--- src/components/FourthStepWindow.tsx
+++ src/components/FourthStepWindow.tsx
@@ -1,56 +1,53 @@
 'use client'
 import { useContext } from "react";
 import FourthStepWindowHeader from "./FourthStepWindowHeader";
-import { UserContext } from "./ContextProvider";
+import { User, UserContext } from "./ContextProvider";
+
 
 const FourthStepWindow = () => {
     const {user} = useContext(UserContext);
 
-    function getTotal() {
-        let total = 0;
-        if(user.billingType === "yearly arcade"){
-            total += 90;
+    function getTotal(user: User): number {
+        const basePriceMap = {
+            "yearly arcade": 90,
+            "monthly arcade": 9,
+            "yearly advanced": 120,
+            "monthly advanced": 12,
+            "yearly pro": 150,
+            "monthly pro": 15,
+        };
+    
+        const additionalMonthlyPrices = {
+            customizableProfile: 2,
+            largerStorage: 2,
+            onlineService: 1,
+        };
+    
+        const additionalYearlyPrices = {
+            customizableProfile: 20,
+            largerStorage: 20,
+            onlineService: 10,
+        };
+    
+        let total = basePriceMap[user.billingType] || 0;
+    
+        if (user.billingType.includes("monthly")) {
+            total += Object.keys(additionalMonthlyPrices)
+                .filter(option => user[option])
+                .reduce((sum, option) => sum + additionalMonthlyPrices[option], 0);
+        } else {
+            total += Object.keys(additionalYearlyPrices)
+                .filter(option => user[option])
+                .reduce((sum, option) => sum + additionalYearlyPrices[option], 0);
         }
-        if(user.billingType === "monthly arcade"){
-            total += 9;
-        }
-        if(user.billingType === "yearly advanced"){
-            total += 120;
-        }
-        if(user.billingType === "monthly advanced"){
-            total += 12;
-        }
-        if(user.billingType === "yearly pro"){
-            total += 150;
-        }
-        if(user.billingType === "monthly pro"){
-            total += 15;
-        }
-        if(user.customizableProfile && user.billingType.includes("monthly")){
-            total+= 2;
-        }
-        if(user.largerStorage && user.billingType.includes("monthly")){
-            total += 2;
-        }
-        if(user.onlineService && user.billingType.includes("monthly")){
-            total += 1;
-        }
-        if(user.customizableProfile && !user.billingType.includes("monthly")){
-            total += 20;
-        }
-        if(user.largerStorage && !user.billingType.includes("monthly")){
-            total += 20;
-        }
-        if(user.onlineService && !user.billingType.includes("monthly")){
-            total += 10;
-        }
-        return total; 
+    
+        return total;
     }
 
     return ( 
         <div className="z-10 bg-White py-4 px-3 rounded-lg mt-[10%] w-[94%]">
             <FourthStepWindowHeader />
-            <p>{getTotal()}</p>
+            <p>{getTotal(user)}</p>
         </div> 
     );
 }
