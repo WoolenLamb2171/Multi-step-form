8c7f873c7b97eafcdb13232f52593fe49cfe47f1
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-22 21:29:07 +0300

    add inputs to third step form

--- src/components/ThirdStepForm.tsx
+++ src/components/ThirdStepForm.tsx
@@ -1,18 +1,36 @@
 'use client';
 import { useForm } from "react-hook-form";
 import ThirdStepFormHeader from "./ThirsStepFormHeader";
+interface FormData {
+    onlineService: boolean;
+    largerStorage: boolean;
+    customizableProfile: boolean;
+}
 
 const ThirdStepForm = () => {
-    interface FormData {
-        addsOns: string
-    }
 
-    const {register, getValues} = useForm<FormData>();
+    const {register, getValues, handleSubmit} = useForm<FormData>();
+
+    const handleThirdFormSubmit = () =>{
+        alert(`onlineService:  ${getValues('onlineService')} largerStorage: ${getValues('largerStorage')} customizableProfile: ${getValues('customizableProfile')}`);
+    }
 
     return ( 
         <div className="z-10 bg-White py-4 px-3 rounded-lg mt-[10%] w-[94%]">
             <ThirdStepFormHeader />
+            <form id="ThirdStepForm" onSubmit={handleSubmit(handleThirdFormSubmit)}>
+                <div>
+                    <input type='checkbox' {...register('onlineService')} />
+                </div>
 
+                <div>
+                    <input type='checkbox' {...register('largerStorage')} />
+                </div>
+
+                <div>
+                    <input type='checkbox' {...register('customizableProfile')} />
+                </div>
+            </form>
         </div> 
     );
 }
